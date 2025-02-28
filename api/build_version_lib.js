0265acc227d386d439bbf786e62bdaac60b5819a
hannelorek <hannelor-bdofeu.uv24cd@nozbe.email>
2025-02-28 23:21:35 +0000

    .

---
+++ api/build_version_lib.js
@@ -0,0 +1,9 @@
+export const build_date =      "__BUILD_DATE__";      //  ISO-8601 date
+export const build_timestamp = "__BUILD_TIMESTAMP__"; //  ISO-8601 timestamp
+export const commit_ref =      "__COMMIT_REF__";      //  commit reference hash
+
+/* sample usage:
+import { build_date } from './build_version_lib.js';
+// "x-version": build_date,
+*/
+
