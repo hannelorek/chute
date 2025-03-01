2157c95433e223f19d06c4b125d7119918e169f2
hannelorek <hannelor-bdofeu.uv24cd@nozbe.email>
2025-03-01 13:02:38 +0000

    .

--- api/qrcode_svg.js
+++ api/qrcode_svg.js
@@ -6,18 +6,18 @@ returns:
 export default function(req, res) {
   let key = (req && req.query && req.query.key ? req.query.key : "unknown_key");
   let endpoint = (req && req.query && req.query.endpoint ? req.query.endpoint : "unknown_endpoint");
-  let hostname = (req && req.query && req.query.hostname ? req.query.hostname : "unknown-hostname");
-  let path = (req && req.query && req.query.path ? req.query.path : "unknown_path");
+  let hostname = (req && req.hostname ? req.hostname : "unknown-hostname");
+  let path = (req && req.query && req.path ? req.path : "unknown_path");
 
   var QRCode = require("qrcode-svg");
   var qrcode = new QRCode({
     content: 
       "https://" + hostname +
-      path.replace(/^\/*$/,"") +
+      path.replace(/[^\/]*$/,"") +
       endpoint + "?key=" + encodeURIComponent(key),
     join: true,
     container: "svg-viewbox" //Useful but not required
-  });
+  }).svg();
 
   return (qrcode);
 }
