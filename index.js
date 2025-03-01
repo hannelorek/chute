5dc5ad5b03ce4ca5d59f58b086470ec18cceedb4
hannelorek <hannelor-bdofeu.uv24cd@nozbe.email>
2025-03-01 04:31:20 +0000

    .

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ app.get ('/api/qrcode_svg', async (req, res) => {
 app.get ('/api/read_item', async (req, res) => {
   res.set('x-version', `${build_date}`);
   const funres = await api_read_item(req, res); 
-  res.set('x-funres', JSON.stringify(funres||"XX"));
-  res.json(funres); 
+  res.send(funres);  // HTML code
 });
 app.post('/api/write_item', async (req, res) => {
   res.set('x-version', `${build_date}`);
