fa2035da33756eea9688717263b067e2bb9f6950
hannelorek <hannelor-bdofeu.uv24cd@nozbe.email>
2025-03-01 03:25:21 +0000

    .

---
+++ api/read_write_item.js
@@ -0,0 +1,52 @@
+/*
+curl -X GET  -H "X-api-key: x" .../api/read_write_item?action=read&key=abc123
+curl -X POST -H "X-api-key: x" .../api/read_write_item?action=read&key=abc123 \
+  -H 'content-type: application/json' --data '{"data":"abcd"}'
+
+returns:
+  
+*/
+
+export async function api_read_item(req, res) {
+  let key = (req && req.query && req.query.key ? req.query.key : "unknown_key");
+
+  import {
+    Datastore
+  } from 'codehooks-js';
+  const conn = await Datastore.open();
+  const kval = await conn.get(key);
+
+  const HTML1 = '<!DOCTYPE html><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-us"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>������</title></head><body><pre>';
+  const HTML2 = '</pre></body></html>';
+
+  return (HTML1 + escapeHtml(kval) + HTML2);
+
+
+  function escapeHtml(text) {
+    const entities = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#39;'
+    };
+    return text.replace(/[&<>"']/g, (match) => entities[match]);
+  }
+
+}
+
+export async function api_write_item(req, res) {
+  let key = (req && req.query && req.query.key ? req.query.key : "unknown_key");
+
+  import {
+    Datastore
+  } from 'codehooks-js';
+  const conn = await Datastore.open();
+  const result = await conn.set(
+    key,
+    (req && req.body && req.body.data ? req.body.data : '')
+  );
+
+  return (result);
+
+}
