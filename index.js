330518f95e1391110e39e75e9120af9ec4ea6ea3
hannelorek <hannelor-bdofeu.uv24cd@nozbe.email>
2025-03-01 04:45:46 +0000

    .

--- index.js
+++ index.js
@@ -34,14 +34,14 @@ app.get ('/api/qrcode_svg', async (req, res) => {
 });
 app.get ('/api/read_item', async (req, res) => {
   res.set('x-version', `${build_date}`);
-  const funres = await api_read_item(req, res); 
   res.set('content-type', 'text/html; charset=utf-8');
+  const funres = await api_read_item(req, res); 
   res.send(funres);  // HTML code
 });
 app.post('/api/write_item', async (req, res) => {
   res.set('x-version', `${build_date}`);
-  const funres = await api_write_item(req, res); 
   res.set('content-type', 'text/plain; charset=utf-8');
+  const funres = await api_write_item(req, res); 
   res.send(funres);  // stored value
 });
 
